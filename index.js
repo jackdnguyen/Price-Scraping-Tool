e0155106aa48e252a1c358ef09d86349d993518e
Sameer Ali <ssa373@sfu.ca>
2022-07-03 20:19:31 -0700

    done url page login

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
