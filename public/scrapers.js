7ccd43bc750243a2d71743e4e56566cae4fcc5b4
jdn8 <jack_nguyen@sfu.ca>
2022-07-03 21:21:11 -0700

    updated goemans - power usage increases ~3000 product

--- public/scrapers.js
+++ public/scrapers.js
@@ -1,144 +1,121 @@
+// Author: Jack Nguyen
+// Purpose: Scrapes data from goemans.com
 const puppeteer = require('puppeteer');
-const Array = [];
+const urlArray = [];
+const results = [];
 
-var goemansProduct = {
-     name:'',
-     sku:'',
-     url:'',
-     lastmod:'',
-     price:''
-};
+let browser;
 
+// Function Parameters: URL, Last Modified Date, Product num
+// Scrapes Price, SKU, Product Name from Product URL
 async function scrapeProduct(url, lastmod, i) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    page.goto(url);
-
-    await page.waitForXPath('/html/body/div[3]/div/div[2]/div[1]');
-    console.log(`Product ${i}`)
-    // Try block in-case xPath doesn't exist
-    // For cases where no specific product
-    try{
-        const [el] = await page.$x('/html/body/div[3]/div/div[2]/div[1]/span');
-        const txt = await el.getProperty('innerText');
-        const rawTxt = await txt.jsonValue();
-        if(rawTxt == 'Filters'){
-            console.log("No product");
-            await page.waitForTimeout(1500);
-            await browser.close();
-            return
-        }
-    } catch(e){}
     // Retrieves SKU from URL
-    urlSplit = url.split("/");
-    sku = urlSplit[urlSplit.length-1]
-
-    // For cases where no pricing
+    const urlSplit = url.split("/");
+    const sku = urlSplit[urlSplit.length-1]
+    if(sku == ''){ // If URL is not a product exit
+        return;
+    }
+    // Set-up return object
+    var obj = {
+        Product:`${i}`,
+        name:'',
+        sku:`${sku}`,
+        url:`${url}`,
+        lastmod:`${lastmod}`,
+        price:''
+   };
+   const page = await browser.newPage();
+   page.goto(url, {
+    waitUntil: 'load',
+    //remove timout
+    timeout: 0
+   });
+   process.setMaxListeners(Infinity); // Sets Max Listeners to Inf
     try{
-        const [el] = await page.$x('/html/body/div[3]/div/div[2]/div[3]/div[2]/div[2]/dl/dt/a');
-        const txt = await el.getProperty('innerText');
-        const price = await txt.jsonValue();
-
-        if(price=='Please Contact Store for Pricing'){
-            console.log("No Pricing");
-
-            await page.waitForXPath('/html/body/div[3]/div/div[2]/div[3]/div[2]/div[2]/h2');
-            const [el2] = await page.$x('/html/body/div[3]/div/div[2]/div[3]/div[2]/div[2]/h2');
-            const txt2 = await el2.getProperty('innerText');
-            const ProductName = await txt2.jsonValue();
-            console.log(sku);
-            console.log(ProductName);
-            await browser.close();
-            return
-        }
-    } catch (e){} 
-    try { // Cases for goeman's displays Mielle's pricing
-        const [elem] = await page.$x('//*[@id="terms"]');
-        const mielleTxt = await elem.getProperty('innerText');
-        const mielle = await mielleTxt.jsonValue();
-        if(mielle == 'What is this?'){
-            console.log("No Pricing");
-            // Product Name
-            await page.waitForXPath('/html/body/div[3]/div/div[2]/div[3]/div[2]/div[2]/h2');
-            const [el3] = await page.$x('/html/body/div[3]/div/div[2]/div[3]/div[2]/div[2]/h2');
-            const txt3 = await el3.getProperty('innerText');
-            const productName = await txt3.jsonValue();
-            console.log(sku);
-            console.log(productName);
-            await browser.close();
-            return
-        }
-    } catch(e){}
-    // Pricing exists
-        // Extract Price
-        const [el] = await page.$x('/html/body/div[3]/div/div[2]/div[3]/div[2]/div[2]/dl[2]/dd');
-        const txt = await el.getProperty('innerText');
-        const rawTxt = await txt.jsonValue();
-        let text = rawTxt.replace(/\$|,/g,''); // Turn price into integer value
-        
         // Extract Product Name
         await page.waitForXPath('/html/body/div[3]/div/div[2]/div[3]/div[2]/div[2]/h2');
         const [el3] = await page.$x('/html/body/div[3]/div/div[2]/div[3]/div[2]/div[2]/h2');
         const txt3 = await el3.getProperty('innerText');
         const rawTxt3 = await txt3.jsonValue();
+        obj.name = rawTxt3;
+        try{
+            // Extract Price
+            const [el] = await page.$x('/html/body/div[3]/div/div[2]/div[3]/div[2]/div[2]/dl[2]/dd');
+            const txt = await el.getProperty('innerText');
+            const rawTxt = await txt.jsonValue();
+            let text = rawTxt.replace(/\$|,/g,''); // Turn price into integer value
+            obj.price = parseFloat(text);
+        }catch(e){ // Price Doesn't exist
+            obj.price = 0;
+        }
+        results.push(obj); // Push Obj into results array
+        await page.close();
+    } catch(e){
+        console.log(e);
+        page.close();
+    } finally {
+        console.log(obj);
+    }
+}
+//scrapeProduct('https://www.goemans.com/home/kitchen/accessories/cooking/range/OW3001');
 
-        console.log(parseFloat(text));
-        console.log(sku);
-        console.log(rawTxt3);
+// Function runs through goemans.com/sitemap.xml to extract all of product URL's
+async function sitemap(index){
+    const browser = await puppeteer.launch();
+    try{
+        const page = await browser.newPage();
+        page.goto('https://www.goemans.com/sitemap.xml');
+        page.setDefaultNavigationTimeout(0); // Sets navigation limit to inf
+        await page.waitForNavigation({ // Wait until network is idle
+            waitUntil: 'networkidle0',
+        });
+        // Loop: Extracts URL & lastmod from sitemap
+        var i = index;
+        for(var i;i<5000;i++){
+            // Extracts url
+            await page.waitForSelector(`#folder${i} > div.opened > div:nth-child(2) > span:nth-child(2)`, { // Wait for selector to laod
+                visible: true,
+            });
+            const [el] = await page.$$(`#folder${i} > div.opened > div:nth-child(2) > span:nth-child(2)`);
+            const txt = await el.getProperty('innerText');
+            const url = await txt.jsonValue();
+            // Extracts lastmod
+            await page.waitForSelector(`#folder${i} > div.opened > div:nth-child(4) > span:nth-child(2)`, {
+                visible: true,
+            });
+            const [el2] = await page.$$(`#folder${i} > div.opened > div:nth-child(4) > span:nth-child(2)`);
+            const txt2 = await el2.getProperty('innerText');
+            const lastmod = await txt2.jsonValue();
+            // Create Object and push into Url Array
+            let obj = {
+                url:`${url}`,
+                lastmod:`${lastmod}`,
+            }
+            urlArray.push(obj);
+        }
+    } catch(e) {
+        console.log(e);
+    } finally {
         await browser.close();
+        scrape(); // Run Scrape URL Function
+    }
 }
 
-//scrapeProduct('https://www.goemans.com/home/kitchen/cooking/range/gas/HR-1124-3-LP-AG');
-
-async function sitemap(){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    page.goto('https://www.goemans.com/sitemap.xml');
-    page.setDefaultNavigationTimeout(0); // Sets navigation limit to inf
-    await page.waitForNavigation({ // Wait until network is idle
-        waitUntil: 'networkidle0',
-      });
-    // Loop: Extracts URL & lastmod from sitemap
-    for(var i=350;i<450;i++){
-        // Extracts url
-        await page.waitForSelector(`#folder${i} > div.opened > div:nth-child(2) > span:nth-child(2)`, { // Wait for selector to laod
-            visible: true,
-        });
-        const [el] = await page.$$(`#folder${i} > div.opened > div:nth-child(2) > span:nth-child(2)`);
-        const txt = await el.getProperty('innerText');
-        const url = await txt.jsonValue();
-        // Extracts lastmod
-        await page.waitForSelector(`#folder${i} > div.opened > div:nth-child(4) > span:nth-child(2)`, {
-            visible: true,
-        });
-        const [el2] = await page.$$(`#folder${i} > div.opened > div:nth-child(4) > span:nth-child(2)`);
-        const txt2 = await el2.getProperty('innerText');
-        const lastmod = await txt2.jsonValue();
-
-        scrapeProduct(url,lastmod, i);
-        await page.waitForTimeout(1500);
+function printArray(){
+    for(var i=0;i<Array.length;i++){
+        console.log(Array[i].url);
     }
-    //await page.waitForTimeout(3000);
-    // for(var i=415;i<9000;i++){
-    //     // Extracts url
-    //     await page.waitForSelector(`#folder${i} > div.opened > div:nth-child(2) > span:nth-child(2)`, { // Wait for selector to laod
-    //         visible: true,
-    //     });
-    //     const [el] = await page.$$(`#folder${i} > div.opened > div:nth-child(2) > span:nth-child(2)`);
-    //     const txt = await el.getProperty('innerText');
-    //     const url = await txt.jsonValue();
-    //     // Extracts lastmod
-    //     await page.waitForSelector(`#folder${i} > div.opened > div:nth-child(4) > span:nth-child(2)`, {
-    //         visible: true,
-    //     });
-    //     const [el2] = await page.$$(`#folder${i} > div.opened > div:nth-child(4) > span:nth-child(2)`);
-    //     const txt2 = await el2.getProperty('innerText');
-    //     const lastmod = await txt2.jsonValue();
+}
 
-    //     scrapeProduct(url,lastmod, i);
-    //     await page.waitForTimeout(1250);
-    // }
+const timer = ms => new Promise(res => setTimeout(res, ms)) // Creates a timeout using promise
+// Runs Scrape Product for each element in URL Array
+async function scrape(){
+    browser = await puppeteer.launch();
+    for(var i=0; i<urlArray.length;i++){
+        scrapeProduct(urlArray[i].url, urlArray[i].lastmod, i);
+        await timer(1400); // 1.4 second delay
+    }
     await browser.close();
 }
 
-sitemap();
+sitemap(106);
