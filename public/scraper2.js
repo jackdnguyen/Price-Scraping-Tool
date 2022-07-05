db7970279de40d98883540350de8d5b73f647e83
tsakib <tosrif_jahan_sakib@sfu.ca>
2022-07-04 18:12:55 -0700

    test for 100 links worked

--- public/scraper2.js
+++ public/scraper2.js
@@ -1,23 +1,43 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 
-let results = []
+function delay(time) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
+ }
 
 async function sitemap1() {
    try {
-       const URL = 'https://www.canadianappliance.ca/sitemaps/sitemap-1.xml'
-       const browser = await puppeteer.launch()
+       const URL = 'https://www.canadianappliance.ca/_sitemap_products.php'
+       const browser = await puppeteer.launch(
+        {
+            // userDataDir: './data',
+            headless: true,
+            args: ['--no-sandbox']
+        }
+       )
        const page = await browser.newPage()
 
-       await page.goto(URL)
+       await page.goto(URL,{
+        waitUntil: 'load',
+        timeout: 0,
+       })
        let data = await page.evaluate(() => {
            let urls = []
-           let items = document.getElementsByTagName('loc')
-            for(let i= 0; i < 1; i++) {
-                urls.push(items[i].innerHTML)
+           let items = document.getElementsByTagName('body')[0].innerHTML.split('\n')
+            for(let i= 0; i < 100; i++) {
+                urls.push(items[i])
             }
             return urls
        })
-       console.log(data)
+    //    console.log(data)
+    //    data.forEach(async(item)=>{
+    //     await scraper(browser, item)
+    //    })
+       for(let i = 0; i < 100; i++) {
+        await scraper(browser, data[i], i)
+        delay(1000)
+       }
        await browser.close()
    } 
    catch (error) {
@@ -25,37 +45,64 @@ async function sitemap1() {
    }
 }
 
-async function scraper(link) {  
+//data2 = data.slice(1, data.length-4)
+//data3 = data2.replace(',' , '')
+//parseFloat(data3)
+
+
+async function scraper(browser, link, index) {  
     try {
         const URL = link
-        const browser = await puppeteer.launch()
         const page = await browser.newPage()
- 
-        await page.goto(URL)
+
+        await page.goto(URL, { 
+            waitUntil: 'networkidle2',
+            timeout: 0})
+
         let data = await page.evaluate(() => {
+            if(document.querySelector('h1 [itemprop=name]') == null)
+                return
+            else {
+                let results = []
+                let tempPrice = 0
+
+                if(document.querySelector('[itemprop=price]') != null){ 
+                    tempPrice = document.querySelector('[itemprop=price]').textContent
+                    tempPrice = parseFloat(tempPrice.slice(1, tempPrice.length-4).replace(',' , ''))
+                }
                     results.push({
-                    name: document.querySelector('h1 [itemprop=name]').textContent,
-                    sku: document.querySelector('h1 div').textContent,
-                    price: document.querySelector('[itemprop=price]').textContent,
-                })
+                        name: document.querySelector('h1 [itemprop=name]').textContent,
+                        sku: document.querySelector('h1 div').textContent,
+                        price: tempPrice,
+                    })
+                    return results
+            }
+        })
 
-            })
-
+        if(data == null) {
+            console.log(index)
+            await page.close
+        }
+        else {
+            console.log(index)
             console.log(data)
-            await browser.close()
-        } 
+            await page.close
+        }
+    } 
     catch (error) {
             console.error(error)
     }
 }
 
+sitemap1()
+
 // async function runScrap() {
-    // sitemap1()
-    // await console.log(sitemap1())
+//     sitemap1()
+//     await console.log(links)
 // }
 
 // runScrap()
-sitemap1()
+// sitemap1()
 // await console.log(sitemap1())
 
 
