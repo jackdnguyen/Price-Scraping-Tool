a909ed25f9dbb951ad4beb1cbd039c1b29986ddf
danielv <daniel_vuksan@sfu.ca>
2022-07-06 08:48:45 +0000

    Update index.js login logout functionality added and linking between ejs files

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
-const { Pool } = require("pg");
-var pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT
 
 app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// session info
 app.use(
   session({
     name: "session",
-    secret: "zordon",
+    secret: "bigpete",
     resave: false,
     saveUninitialized: false,
-    maxAge: 30 * 60 * 1000, // 30 minutes
+    maxAge: 20 * 1000, // 30 minutes
   })
 );
 
-app.use(express.static(path.join(dirname, "public")));
-app.set("views", path.join(dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.get("/", (req, res) => res.render("pages/index"));
 
 app.post("/login", async (req, res) => {
   var un = req.body.f_uname;
   var pwd = req.body.f_pwd;
+
+  // var verifyQuery = `SELECT * FROM validusers WHERE uname='${un}' AND password='${pwd}'`
+  // var result = await pool.query(verifyQuery)
+
+  // if result.rows is !empty
+  // valid user
   if (un === "admin" && pwd === "scrapper") {
     // valid
     req.session.user = req.body;
@@ -37,12 +40,29 @@ app.post("/login", async (req, res) => {
   } else {
     res.redirect("/login.html");
   }
+  // else
+  // invalid user
+});
+
+app.post("/logout", async (req, res) => {
+  console.log("hello");
+  res.redirect("/login.html");
+  req.session.destroy();
+  // else
+  // invalid user
 });
 
 app.get("/dashboard", (req, res) => {
-  if (req.session.user)
-    res.send(hello valid user ${req.session.user.f_uname}!);
-  else res.redirect("/login.html");
+  if (req.session.user) {
+    res.render("pages/urlPage");
+  } else res.redirect("/login.html");
 });
-
-app.listen(PORT, () => console.log(Listening on ${PORT}));
+app.post("/display", async (req, res) => {
+  console.log("hello");
+  //res.render("pages/display");
+  res.render("pages/companyData");
+  res.redirect("/companyData");
+  // else
+  // invalid user
+});
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
